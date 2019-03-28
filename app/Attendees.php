d833beae666c588bb04fcd25ccb7b24e85514671
Simon <simon.fraipont@gmail.com>
2019-03-28 11:48:47 +0100

    seed working

--- app/Attendees.php
+++ app/Attendees.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendees extends Model
 {
-    //
+    protected $primaryKey = null;
+    public $incrementing = false;
+
+    protected $fillable = [
+      'event_id',
+      'user_id',
+    ];
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,11 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+     public function run()
+     {
+         $this->call(UserSeeder::class);
+         $this->call(EventSeeder::class);
+         $this->call(AttendeeSeeder::class);
+
+     }
 }
