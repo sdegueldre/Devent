945cbda561633d95be3d66d21e2d6db2de85629d
VieuxLoupGris <provocemois@gmail.com>
2019-04-09 14:13:23 +0200

    Petite modification header

--- resources/js/components/pages/contact.js
+++ resources/js/components/pages/contact.js
@@ -11,15 +11,29 @@ export default class Contact extends Component {
               <h1 className = "mt-3 mb-2 text-white" > < i className = "far fa-id-card" > < /i> Contact</h1 >
             </div>
 
-              <div className="row py-5 mx-auto">
+              <div className="row col-12 py-5 mx-auto">
 
-                <div className="col-4 col-lg-4 col-md-5 d-flex flex-wrap align-items-start mb-5 py-5 px-5 mx-auto">
-                  <h2 className = " mt-3 mb-2 text-white h3 " > < i className = "far fas fa-at" > < /i> e-mail</h2 >
+                <div className="col-sm-12 col-md-6 col-lg-4 d-flex flex-wrap align-items-start mb-3 py-4 px-5 ">
+                  <h2 className = "mt-3 mb-2 text-white h3 " > < i className = "far fas fa-at" > < /i> e-mail</h2 >
                   <a href="#"><p className="h4 pl-4 pb-3">devevent@gmail.com</p></a>
                 </div>
 
-                <div className="col-4 col-lg-4 col-md-5 d-flex flex-wrap align-items-start mb-5 py-5 px-5 mx-auto">
-                  <h2 className = " mt-3 mb-2 text-white h3" > < i className = "fas fa-phone-square" > < /i> phone number</h2 >
+                <div className="col-sm-12 col-md-6 col-lg-4 d-flex flex-wrap align-items-start mb-3 py-4 px-5 mx-auto">
+                  <h2 className = "mt-3 mb-2 text-white h3" > < i className = "fas fa-phone-square" > < /i> phone number</h2 >
+                  <p className="h4 pl-4 pb-3 text-danger">+32 456/789.123</p>
+                </div>
+
+              </div>
+
+              <div className="row col-12 py-3 mx-auto">
+
+                <div className="col-sm-12 col-md-6 col-lg-4 d-flex flex-wrap align-items-start mb-3 py-4 px-5 ">
+                  <h2 className = "mt-3 mb-2 text-white h3 " > < i className = "far fas fa-at" > < /i> e-mail</h2 >
+                  <a href="#"><p className="h4 pl-4 pb-3">devevent@gmail.com</p></a>
+                </div>
+
+                <div className="col-sm-12 col-md-6 col-lg-4 d-flex flex-wrap align-items-start mb-3 py-4 px-5 mx-auto">
+                  <h2 className = "mt-3 mb-2 text-white h3" > < i className = "fas fa-phone-square" > < /i> phone number</h2 >
                   <p className="h4 pl-4 pb-3 text-danger">+32 456/789.123</p>
                 </div>
 
