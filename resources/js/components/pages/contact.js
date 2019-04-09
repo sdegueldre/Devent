b6f65d81918a8993f7ec9524a92e57d328839e32
VieuxLoupGris <provocemois@gmail.com>
2019-04-09 09:40:27 +0200

    ClassName Error Fixed

--- resources/js/components/pages/contact.js
+++ resources/js/components/pages/contact.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {
-    Link
-} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 export default class Contact extends Component {
     render() {
@@ -10,19 +8,19 @@ export default class Contact extends Component {
           <div className = "container" >
 
             <div className = "row text-white mx-auto pb-4" >
-              <h1 className = "mt-3 mb-2 text-white" > < i class = "far fa-id-card" > < /i> Contact</h1 >
+              <h1 className = "mt-3 mb-2 text-white" > < i className = "far fa-id-card" > < /i> Contact</h1 >
             </div>
 
               <div className="row py-5 mx-auto">
 
-                <div className="col-4 col-lg-4 col-md-5 d-flex flex-wrap align-items-start py-5 px-5 mx-auto">
-                  <h2 className = " mt-3 mb-2 text-white h3 " > < i class = "far fas fa-at" > < /i> e-mail</h2 >
-                  <a href="#"><p className="h4 pl-3 pb-3">devevent@gmail.com</p></a>
+                <div className="col-4 col-lg-4 col-md-5 d-flex flex-wrap align-items-start mb-5 py-5 px-5 mx-auto">
+                  <h2 className = " mt-3 mb-2 text-white h3 " > < i className = "far fas fa-at" > < /i> e-mail</h2 >
+                  <a href="#"><p className="h4 pl-4 pb-3">devevent@gmail.com</p></a>
                 </div>
 
-                <div className="col-4 col-lg-4 col-md-5 d-flex flex-wrap align-items-start py-5 px-5 mx-auto">
-                  <h2 className = " mt-3 mb-2 text-white h3" > < i class = "fas fa-phone-square" > < /i> phone number</h2 >
-                  <p className="h4 pl-3 pb-3 text-danger">+32 456/789.123</p>
+                <div className="col-4 col-lg-4 col-md-5 d-flex flex-wrap align-items-start mb-5 py-5 px-5 mx-auto">
+                  <h2 className = " mt-3 mb-2 text-white h3" > < i className = "fas fa-phone-square" > < /i> phone number</h2 >
+                  <p className="h4 pl-4 pb-3 text-danger">+32 456/789.123</p>
                 </div>
 
               </div>
