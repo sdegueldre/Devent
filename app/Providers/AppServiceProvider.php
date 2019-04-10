14e2205fa9a8a5c10faf8d6b3e4605be79f5f00e
NadTr <trauflernadine@gmail.com>
2019-04-10 09:43:00 +0200

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
       if (env('APP_ENV') === 'production') {
-        URL::forceScheme("https");
+        URL::forceSchema("https");
       }
     }
 }
