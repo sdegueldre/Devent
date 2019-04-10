80c80b2b12a0f0fcf8da410270849c97cf292590
Simon <simon.fraipont@gmail.com>
2019-04-10 10:09:09 +0200

    change reminder mail button target

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -4,7 +4,7 @@
 It will take place in *{{$event['event_location']}}* on {{substr($event['event_time'], 0, -9)}} at {{substr($event['event_time'],11, -3)}}.<br>
 We hope to see you there!
 
-@component('mail::button', ['url' =>'http://localhost:8000/event/' . $event['id'], 'color' => 'success'])
+@component('mail::button', ['url' =>'https://jepsen-brite-jnsnc.herokuapp.com/' . $event['id'], 'color' => 'success'])
 Get back to Dev'ent!
 @endcomponent
 
