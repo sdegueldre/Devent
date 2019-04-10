d14fd883bbace078334362bfd16aebe150b980f7
Simon <simon.fraipont@gmail.com>
2019-04-10 09:58:19 +0200

    change host if deployed on heroku

--- config/app.php
+++ config/app.php
@@ -1,5 +1,8 @@
 <?php
-
+$host='http://localhost';
+if(env('APP_ENV', 'production')!='local'){
+  $host = 'https://jepsen-brite-jnsnc.herokuapp.com/');
+}
 return [
 
     /*
@@ -52,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', $host),
 
     'asset_url' => env('ASSET_URL', null),
 
