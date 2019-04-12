3c63ba1f11c35cae0be9402ccf9dc06378403ceb
bovealexandre <alexandre.l.bove@gmail.com>
2019-04-12 09:07:32 +0200

    ajout vidéo event

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'devevent@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Dev\'ent'),
     ],
 
     /*
